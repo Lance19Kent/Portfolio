1cef1413b1e3399528f28314f5f5a802a617da2d
Lance Kent <lancemagollado46@gmail.com>
2026-02-28 20:12:29 +0800

    Navbar done

--- lance-portfolio/src/components/Sidebar.jsx
+++ lance-portfolio/src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ function Sidebar(){
                 <h3 className="text-right text-black text-[18px] font-semibold text-nowrap whitespace-nowrap text-ellipsis overflow-hidden w-full pt-4">Magollado, Lance Kent Geoffrey B.</h3>
                 <h2 className="text-black text-[14px] pb-6.25">Full-Stack Developer</h2>
 
-                <div className="p-2 border-[#01010140] border rounded-[10px] flex gap-2.5 justify-center">
+                <div className="p-2 border-[#01010140] border rounded-[10px] flex gap-2.5 justify-center cursor-pointer">
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M12 15V3" stroke="black" stroke-opacity=".4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="black" stroke-opacity=".4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -62,14 +62,17 @@ function Sidebar(){
                         </li>
                     </ul>
                 </div>
-                <div className="border border-[#D9D9D950] mt-3"></div>
-                <div className="flex flex-row-reverse gap-5 bg-amber-300">
-                    <span className="text-black text-[16px] font-medium">Dark Mode</span>
-                    <Switch />
+                <div className="border border-[#D9D9D950] my-3"></div>
+                <div className="flex flex-row gap-2.5 items-center justify-center my-3">
+                       <Switch className={"cursor-pointer"}/>
+                    <span className="text-black text-[16px]">Dark Mode</span>
+                </div>
+                <div className="mt-4">
+                    <p className="text-[#00000050] text-xs text-nowrap overflow-hidden text-ellipsis">Designed and Built By Lance Kent Geoffrey B. Magollado</p>
+                    <p className="text-[#00000050] text-xs text-nowrap overflow-hidden text-ellipsis">@2025 All Rights Reserved.</p>
                 </div>
             </div>
         </nav>
-
     )
 }
 
