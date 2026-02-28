e8a02c0f5391aa5a3a81d46b05526a5d28478a1b
Lance Kent <lancemagollado46@gmail.com>
2026-02-28 13:31:20 +0800

    Tailwind Installed Succesfully

---
+++ lance-portfolio/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+function Sidebar(){
+    return(
+        <div className="text-red">
+            <h1 className="text-red-500">Sidebar Component Here!</h1>
+        </div>
+    )
+}
+
+export default Sidebar;
