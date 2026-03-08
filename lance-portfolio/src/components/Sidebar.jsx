7bef8cc4f5a2fdec96812976c0e26065833a69a1
Lance Kent <lancemagollado46@gmail.com>
2026-03-08 19:40:56 +0800

    Sidebar Done

--- lance-portfolio/src/components/Sidebar.jsx
+++ lance-portfolio/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Switch } from "./ui/switch"
-import { Button } from "./ui/button"
+import { Button, buttonVariants } from "./ui/button"
 import { NavLink } from "react-router-dom"
+import {cn} from "@/lib/utils"
 
 function Sidebar(){
     return(
@@ -13,14 +14,21 @@ function Sidebar(){
                     <h3 className="text-left text-black text-[18px] font-semibold text-nowrap whitespace-nowrap text-ellipsis overflow-hidden w-full pt-4">Magollado, Lance Kent Geoffrey B.</h3>
                     <h2 className="text-black text-[14px] pb-6.25">Full-Stack Developer</h2>
                     
-                    <Button variant="outline" className={"py-5 font-light rounded-[10px] flex gap-2.5 justify-center cursor-pointer text-[#00000080] hover:text-[#3FA6F4] text-[16px] hover:bg-white hover:border-[#3FA6F4] hover:stroke-[#3FA6F4] stroke-[#00000060] ease-out "}>
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 15V3"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M7 10L12 15L17 10"   stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    Resume
-                    </Button>
+                    <NavLink to={"/resume"} className={({isActive}) =>cn(buttonVariants({variant:"outline"}),
+                        "py-5 font-light rounded-[10px] flex gap-2.5 justify-center cursor-pointer text-[16px] ease-in transition-all duration-75",
+
+                        isActive
+                                ? "text-[#3FA6F4] hover:text-[#3FA6F4] border-[#3FA6F4] stroke-[#3FA6F4] hover:bg-white " 
+                                : "text-[#00000080] bg-white border-gray-200 stroke-[#00000060] hover:text-[#3FA6F4] hover:border-[#3FA6F4] hover:bg-white hover:stroke-[#3FA6F4]" // 
+                        )}>
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 15V3"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M7 10L12 15L17 10"   stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        Resume
+                        </NavLink>
+                   
                 </div>
                 <div className="border border-[#D9D9D950] mt-6 mb-3"></div>
                 <div>
