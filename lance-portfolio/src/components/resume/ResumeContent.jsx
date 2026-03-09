812524b7adbc183e151a992e100b2e4ee71c6616
Lance Kent <lancemagollado46@gmail.com>
2026-03-09 18:56:09 +0800

    Adding Download Btn

--- lance-portfolio/src/components/resume/ResumeContent.jsx
+++ lance-portfolio/src/components/resume/ResumeContent.jsx
@@ -12,13 +12,16 @@ function ResumeContent(){
     return (
         <div className="w-full pr-3 py-3 overflow-y-auto no-scrollbar overflow-hidden"> 
             <BentoCard title={"Resume"} icon={resumeIcon} className="w-full min-h-200 relative"> 
-                <Button variant="outline" className={"absolute top-5 right-6 hover:text-black/50 cursor-pointer p-6 text-[16px] text-black/50"}>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 15V3" stroke="black" stroke-opacity="0.7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="black" stroke-opacity="0.7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M7 10L12 15L17 10" stroke="black" stroke-opacity="0.7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    <span>Download</span>
+                <Button variant="outline" className={"absolute top-5 right-6 hover:text-black/50 cursor-pointer p-6 text-[16px] text-black/50"} asChild>
+                    <a href="/Magollado-Lance-Kent-Geoffrey-Resume.pdf" download={"Lance Kent Geoffrey B. Magollado's Resume"}>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 15V3" stroke="black" stroke-opacity="0.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="black" stroke-opacity="0.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M7 10L12 15L17 10" stroke="black" stroke-opacity="0.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span>Download</span>
+                    </a>
+
                     </Button>
             </BentoCard>
         </div>
