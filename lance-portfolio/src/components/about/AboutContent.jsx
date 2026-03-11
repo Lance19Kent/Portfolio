1831af19e9c752efe8617be4b8d59fab837929d8
Lance Kent <lancemagollado46@gmail.com>
2026-03-11 21:13:30 +0800

    Adding Certificate Section in About Page

--- lance-portfolio/src/components/about/AboutContent.jsx
+++ lance-portfolio/src/components/about/AboutContent.jsx
@@ -1,5 +1,5 @@
 import BentoCard from "../BentoCard";
-
+import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 import * as React from "react"
 import { Card, CardContent } from "/src/components/ui/card"
@@ -10,6 +10,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "/src/components/ui/carousel"
+
 const aboutIcon = <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M25.3334 28V25.3333C25.3334 23.9188 24.7715 22.5623 23.7713 21.5621C22.7711 20.5619 21.4146 20 20.0001 20H12.0001C10.5856 20 9.22904 20.5619 8.22884 21.5621C7.22865 22.5623 6.66675 23.9188 6.66675 25.3333V28" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M16.0001 14.6667C18.9456 14.6667 21.3334 12.2789 21.3334 9.33333C21.3334 6.38781 18.9456 4 16.0001 4C13.0546 4 10.6667 6.38781 10.6667 9.33333C10.6667 12.2789 13.0546 14.6667 16.0001 14.6667Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -19,6 +20,11 @@ const highlightsIcon =   <svg width="30" height="30" viewBox="0 0 30 30" fill="n
                 <path d="M15 21.25V27.5" stroke="black" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
                 <path d="M11.25 13.45C11.2498 13.9151 11.1198 14.3709 10.8747 14.7662C10.6295 15.1615 10.279 15.4805 9.8625 15.6875L7.6375 16.8125C7.22098 17.0195 6.87046 17.3385 6.62535 17.7338C6.38024 18.1291 6.25025 18.5849 6.25 19.05V20C6.25 20.3315 6.3817 20.6495 6.61612 20.8839C6.85054 21.1183 7.16848 21.25 7.5 21.25H22.5C22.8315 21.25 23.1495 21.1183 23.3839 20.8839C23.6183 20.6495 23.75 20.3315 23.75 20V19.05C23.7498 18.5849 23.6198 18.1291 23.3747 17.7338C23.1295 17.3385 22.779 17.0195 22.3625 16.8125L20.1375 15.6875C19.721 15.4805 19.3705 15.1615 19.1253 14.7662C18.8802 14.3709 18.7502 13.9151 18.75 13.45V8.75C18.75 8.41848 18.8817 8.10054 19.1161 7.86612C19.3505 7.6317 19.6685 7.5 20 7.5C20.663 7.5 21.2989 7.23661 21.7678 6.76777C22.2366 6.29893 22.5 5.66304 22.5 5C22.5 4.33696 22.2366 3.70107 21.7678 3.23223C21.2989 2.76339 20.663 2.5 20 2.5H10C9.33696 2.5 8.70107 2.76339 8.23223 3.23223C7.76339 3.70107 7.5 4.33696 7.5 5C7.5 5.66304 7.76339 6.29893 8.23223 6.76777C8.70107 7.23661 9.33696 7.5 10 7.5C10.3315 7.5 10.6495 7.6317 10.8839 7.86612C11.1183 8.10054 11.25 8.41848 11.25 8.75V13.45Z" stroke="black" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>;
+
+const certificateIcon = <svg width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M25.7951 21.4833L28.3201 35.6933C28.3484 35.8606 28.3249 36.0326 28.2528 36.1862C28.1807 36.3398 28.0634 36.4678 27.9166 36.5529C27.7698 36.6381 27.6006 36.6764 27.4314 36.6628C27.2623 36.6492 27.1013 36.5842 26.9701 36.4766L21.0034 31.9983C20.7154 31.7831 20.3655 31.6668 20.0059 31.6668C19.6464 31.6668 19.2964 31.7831 19.0084 31.9983L13.0317 36.4749C12.9006 36.5823 12.7398 36.6472 12.5709 36.6608C12.402 36.6745 12.2329 36.6363 12.0862 36.5514C11.9395 36.4665 11.8222 36.3388 11.7499 36.1855C11.6777 36.0322 11.6539 35.8605 11.6817 35.6933L14.2051 21.4833" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M20 23.3334C25.5228 23.3334 30 18.8562 30 13.3334C30 7.81053 25.5228 3.33337 20 3.33337C14.4772 3.33337 10 7.81053 10 13.3334C10 18.8562 14.4772 23.3334 20 23.3334Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>;
 
 function AboutContent(){
     return(
@@ -56,29 +62,57 @@ function AboutContent(){
                     Front-End Development is my current playground where logic meets creativity. I love building intuitive user interfaces and crafting smooth digital experiences. Being a Full-Stack Developer is my goal so that I can create an innovative web app of my own.
                 </span>
             </BentoCard>
-            <BentoCard title={"Highlights"} icon={highlightsIcon} className="min-h-100">
+            <BentoCard title={"Highlights"} icon={highlightsIcon} className="">
                 <Carousel
-      opts={{
-        align: "start",
-      }}
-      className="w-full max-w-[12rem] sm:max-w-xs md:max-w-sm"
-    >
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+                  opts={{
+                    align: "start",
+                  }}
+                  className="w-full max-w-[12rem] sm:max-w-xs md:max-w-sm"
+                  >
+                <CarouselContent>
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index} className="basis-1/2 lg:basis-1/3">
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex aspect-square items-center justify-center p-6">
+                            <span className="text-3xl font-semibold">{index + 1}</span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+              </Carousel>
+            </BentoCard>
+            <BentoCard title={"Certificates"} icon={certificateIcon} className="">
+              <div className="flex flex-col gap-4">
+                <Carousel
+                opts={{
+                  align: "start",
+                }}
+                className="w-full max-w-[12rem] sm:max-w-xs md:max-w-sm"
+                >
+                  <CarouselContent>
+                    {Array.from({ length: 5 }).map((_, index) => (
+                      <CarouselItem key={index} className="basis-1/2 lg:basis-1/3">
+                        <div className="p-1">
+                          <Card>
+                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                              <span className="text-3xl font-semibold">{index + 1}</span>
+                            </CardContent>
+                          </Card>
+                        </div>
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
+                  <CarouselPrevious />
+                  <CarouselNext />
+                </Carousel>
+                <Link to={"/achievements"} className="text-center block text-[#3FA6F4] hover:underline">See More</Link>
+              </div>
+               
             </BentoCard>
         </div>
     )
