fa8f16253e41c057d1c877d94f198356cb1a943d
Lance Kent <lancemagollado46@gmail.com>
2026-03-13 21:54:36 +0800

    Projects Section dobe

--- lance-portfolio/src/components/projects/ProjectsContent.jsx
+++ lance-portfolio/src/components/projects/ProjectsContent.jsx
@@ -76,7 +76,7 @@ function ProjectsContent(){
                 </div>
             </BentoCard>  
             <BentoCard title={"Projects"} icon={projectsIcon} className="w-full min-h-[300px] relative gap-8"> 
-                <div className="w-full  flex gap-5 flex-wrap">
+                <div className="w-full  flex gap-x-10 gap-y-5 flex-wrap">
                     <Link className="px-3 w-fit rounded-[10px] py-4 flex items-center border-[.50px] border-[#00000050] cursor-pointer flex-col">
                         <div className="flex gap-2">
                             <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -167,6 +167,61 @@ function ProjectsContent(){
                         </div>
                         
                     </Link>
+                    <Link className="px-3 w-fit rounded-[10px] py-4 flex items-center border-[.50px] border-[#00000050] cursor-pointer flex-col">
+                        <div className="flex gap-2">
+                            <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7.00016 16.3333L8.69183 12.95C8.88662 12.5634 9.18511 12.2387 9.55391 12.012C9.92271 11.7854 10.3473 11.6658 10.7802 11.6667H23.3335M23.3335 11.6667C23.69 11.666 24.0418 11.7471 24.3621 11.9036C24.6823 12.0601 24.9625 12.2879 25.181 12.5695C25.3995 12.8511 25.5506 13.1791 25.6227 13.5281C25.6948 13.8772 25.6859 14.2382 25.5968 14.5833L23.7885 21.5833C23.659 22.0849 23.3663 22.5291 22.9565 22.8459C22.5467 23.1628 22.0432 23.3342 21.5252 23.3333H4.66683C4.04799 23.3333 3.4545 23.0875 3.01691 22.6499C2.57933 22.2123 2.3335 21.6188 2.3335 21V5.83333C2.3335 4.55 3.3835 3.5 4.66683 3.5H9.25183C9.63615 3.50199 10.014 3.59888 10.3519 3.78205C10.6897 3.96523 10.9771 4.22902 11.1885 4.55L12.1452 5.95C12.3565 6.27098 12.6439 6.53477 12.9818 6.71795C13.3196 6.90112 13.6975 6.99801 14.0818 7H21.0002C21.619 7 22.2125 7.24583 22.6501 7.68342C23.0877 8.121 23.3335 8.71449 23.3335 9.33333V11.6667Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M16.3332 18.6667C16.9775 18.6667 17.4998 18.1444 17.4998 17.5C17.4998 16.8557 16.9775 16.3334 16.3332 16.3334C15.6888 16.3334 15.1665 16.8557 15.1665 17.5C15.1665 18.1444 15.6888 18.6667 16.3332 18.6667Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <div className="flex flex-col gap-0.5">
+                                <div className="flex gap-2">
+                                    <span className="font-medium text-[18px]">Bakehub</span>
+                                    <p className="text-[#35BF46] text-[12px] px-2 border py-0.5 rounded-[10px] border-[#C2F49E]">Public</p>
+                                </div>
+                                <p className="font-medium text-[10px] text-[#B9B9B9]">Created on February 19, 2026</p>
+                            </div>
+                           
+                            
+                        </div>
+                        
+                    </Link>
+                    <Link className="px-3 w-fit rounded-[10px] py-4 flex items-center border-[.50px] border-[#00000050] cursor-pointer flex-col">
+                        <div className="flex gap-2">
+                            <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7.00016 16.3333L8.69183 12.95C8.88662 12.5634 9.18511 12.2387 9.55391 12.012C9.92271 11.7854 10.3473 11.6658 10.7802 11.6667H23.3335M23.3335 11.6667C23.69 11.666 24.0418 11.7471 24.3621 11.9036C24.6823 12.0601 24.9625 12.2879 25.181 12.5695C25.3995 12.8511 25.5506 13.1791 25.6227 13.5281C25.6948 13.8772 25.6859 14.2382 25.5968 14.5833L23.7885 21.5833C23.659 22.0849 23.3663 22.5291 22.9565 22.8459C22.5467 23.1628 22.0432 23.3342 21.5252 23.3333H4.66683C4.04799 23.3333 3.4545 23.0875 3.01691 22.6499C2.57933 22.2123 2.3335 21.6188 2.3335 21V5.83333C2.3335 4.55 3.3835 3.5 4.66683 3.5H9.25183C9.63615 3.50199 10.014 3.59888 10.3519 3.78205C10.6897 3.96523 10.9771 4.22902 11.1885 4.55L12.1452 5.95C12.3565 6.27098 12.6439 6.53477 12.9818 6.71795C13.3196 6.90112 13.6975 6.99801 14.0818 7H21.0002C21.619 7 22.2125 7.24583 22.6501 7.68342C23.0877 8.121 23.3335 8.71449 23.3335 9.33333V11.6667Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M16.3332 18.6667C16.9775 18.6667 17.4998 18.1444 17.4998 17.5C17.4998 16.8557 16.9775 16.3334 16.3332 16.3334C15.6888 16.3334 15.1665 16.8557 15.1665 17.5C15.1665 18.1444 15.6888 18.6667 16.3332 18.6667Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <div className="flex flex-col gap-0.5">
+                                <div className="flex gap-2">
+                                    <span className="font-medium text-[18px]">Bakehub</span>
+                                    <p className="text-[#35BF46] text-[12px] px-2 border py-0.5 rounded-[10px] border-[#C2F49E]">Public</p>
+                                </div>
+                                <p className="font-medium text-[10px] text-[#B9B9B9]">Created on February 19, 2026</p>
+                            </div>
+                           
+                            
+                        </div>
+                        
+                    </Link>
+                    <Link className="px-3 w-fit rounded-[10px] py-4 flex items-center border-[.50px] border-[#00000050] cursor-pointer flex-col">
+                        <div className="flex gap-2">
+                            <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7.00016 16.3333L8.69183 12.95C8.88662 12.5634 9.18511 12.2387 9.55391 12.012C9.92271 11.7854 10.3473 11.6658 10.7802 11.6667H23.3335M23.3335 11.6667C23.69 11.666 24.0418 11.7471 24.3621 11.9036C24.6823 12.0601 24.9625 12.2879 25.181 12.5695C25.3995 12.8511 25.5506 13.1791 25.6227 13.5281C25.6948 13.8772 25.6859 14.2382 25.5968 14.5833L23.7885 21.5833C23.659 22.0849 23.3663 22.5291 22.9565 22.8459C22.5467 23.1628 22.0432 23.3342 21.5252 23.3333H4.66683C4.04799 23.3333 3.4545 23.0875 3.01691 22.6499C2.57933 22.2123 2.3335 21.6188 2.3335 21V5.83333C2.3335 4.55 3.3835 3.5 4.66683 3.5H9.25183C9.63615 3.50199 10.014 3.59888 10.3519 3.78205C10.6897 3.96523 10.9771 4.22902 11.1885 4.55L12.1452 5.95C12.3565 6.27098 12.6439 6.53477 12.9818 6.71795C13.3196 6.90112 13.6975 6.99801 14.0818 7H21.0002C21.619 7 22.2125 7.24583 22.6501 7.68342C23.0877 8.121 23.3335 8.71449 23.3335 9.33333V11.6667Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M16.3332 18.6667C16.9775 18.6667 17.4998 18.1444 17.4998 17.5C17.4998 16.8557 16.9775 16.3334 16.3332 16.3334C15.6888 16.3334 15.1665 16.8557 15.1665 17.5C15.1665 18.1444 15.6888 18.6667 16.3332 18.6667Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <div className="flex flex-col gap-0.5">
+                                <div className="flex gap-2">
+                                    <span className="font-medium text-[18px]">Bakehub</span>
+                                    <p className="text-[#35BF46] text-[12px] px-2 border py-0.5 rounded-[10px] border-[#C2F49E]">Public</p>
+                                </div>
+                                <p className="font-medium text-[10px] text-[#B9B9B9]">Created on February 19, 2026</p>
+                            </div>
+                           
+                            
+                        </div>
+                        
+                    </Link>
+                    
                 </div>
             </BentoCard>          
            <ContactMe className="min-h-[200px]"/>
