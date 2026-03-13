42cb57567099c12fcee89fc7b3cff938076a7245
Lance Kent <lancemagollado46@gmail.com>
2026-03-13 12:13:19 +0800

    Featured Projects Section Done

--- lance-portfolio/src/components/projects/ProjectsContent.jsx
+++ lance-portfolio/src/components/projects/ProjectsContent.jsx
@@ -1,13 +1,83 @@
 import BentoCard from "../BentoCard";
 import ContactMe from "../ContactMe";
 
+const featuredIcon =   <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15 21.25V27.5" stroke="black" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M11.25 13.45C11.2498 13.9151 11.1198 14.3709 10.8747 14.7662C10.6295 15.1615 10.279 15.4805 9.8625 15.6875L7.6375 16.8125C7.22098 17.0195 6.87046 17.3385 6.62535 17.7338C6.38024 18.1291 6.25025 18.5849 6.25 19.05V20C6.25 20.3315 6.3817 20.6495 6.61612 20.8839C6.85054 21.1183 7.16848 21.25 7.5 21.25H22.5C22.8315 21.25 23.1495 21.1183 23.3839 20.8839C23.6183 20.6495 23.75 20.3315 23.75 20V19.05C23.7498 18.5849 23.6198 18.1291 23.3747 17.7338C23.1295 17.3385 22.779 17.0195 22.3625 16.8125L20.1375 15.6875C19.721 15.4805 19.3705 15.1615 19.1253 14.7662C18.8802 14.3709 18.7502 13.9151 18.75 13.45V8.75C18.75 8.41848 18.8817 8.10054 19.1161 7.86612C19.3505 7.6317 19.6685 7.5 20 7.5C20.663 7.5 21.2989 7.23661 21.7678 6.76777C22.2366 6.29893 22.5 5.66304 22.5 5C22.5 4.33696 22.2366 3.70107 21.7678 3.23223C21.2989 2.76339 20.663 2.5 20 2.5H10C9.33696 2.5 8.70107 2.76339 8.23223 3.23223C7.76339 3.70107 7.5 4.33696 7.5 5C7.5 5.66304 7.76339 6.29893 8.23223 6.76777C8.70107 7.23661 9.33696 7.5 10 7.5C10.3315 7.5 10.6495 7.6317 10.8839 7.86612C11.1183 8.10054 11.25 8.41848 11.25 8.75V13.45Z" stroke="black" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>;
+
+const projectsIcon = <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M22.5 23.75C20.8424 23.75 19.2527 23.0915 18.0806 21.9194C16.9085 20.7473 16.25 19.1576 16.25 17.5V27.5" stroke="black" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.25 25H5C4.33696 25 3.70107 24.7366 3.23223 24.2678C2.76339 23.7989 2.5 23.163 2.5 22.5V6.25C2.5 5.58696 2.76339 4.95107 3.23223 4.48223C3.70107 4.01339 4.33696 3.75 5 3.75H9.875C10.2931 3.7459 10.7056 3.84673 11.0746 4.04327C11.4437 4.23981 11.7575 4.52578 11.9875 4.875L13 6.375C13.2276 6.72066 13.5375 7.0044 13.9019 7.20075C14.2662 7.39711 14.6736 7.49993 15.0875 7.5H25C25.663 7.5 26.2989 7.76339 26.7678 8.23223C27.2366 8.70107 27.5 9.33696 27.5 10V16.25" stroke="black" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.25 17.5C17.6307 17.5 18.75 16.3807 18.75 15C18.75 13.6193 17.6307 12.5 16.25 12.5C14.8693 12.5 13.75 13.6193 13.75 15C13.75 16.3807 14.8693 17.5 16.25 17.5Z" stroke="black" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M25 26.25C26.3807 26.25 27.5 25.1307 27.5 23.75C27.5 22.3693 26.3807 21.25 25 21.25C23.6193 21.25 22.5 22.3693 22.5 23.75C22.5 25.1307 23.6193 26.25 25 26.25Z" stroke="black" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>;
+
 function ProjectsContent(){
     return(
         <div className="w-full h-full pr-3 py-3 overflow-y-auto no-scrollbar overflow-hidden flex flex-col gap-3"> 
-            <BentoCard title={"Resume"} className="w-full min-h-[200px] relative gap-8"> 
-                     
-            </BentoCard>
-           
+            <BentoCard title={"Featured"} icon={featuredIcon} className="w-full min-h-[400px] relative gap-8"> 
+                <div className="flex items-center justify-between">
+                    <div className="w-[320px] h-[400px] flex flex-col">
+                        <div className="w-full h-[220px] bg-[#ECECEC50] flex flex-col gap-3 rounded-t-[10px] p-5">
+                            <div>
+                                <h1 className="font-semibold text-[24px]">Okay lang</h1>
+                                <div className="flex items-center gap-1">
+                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M6 11C8.76142 11 11 8.76142 11 6C11 3.23858 8.76142 1 6 1C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11Z" stroke="black" stroke-opacity="0.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M6 3V6L5 8" stroke="black" stroke-opacity="0.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                    <span className="font-light text-black/50 text-[12px]">September 09, 2026</span>
+                                </div>
+                            </div>
+                            <p className="text-[12px]">A gentle space where people can check in with themselves without pressure, explanation, or judgment.</p>
+                        </div>
+                        <div className="w-full h-full bg-[#DEDEDE] rounded-b-[10px]">
+
+                        </div>
+                    </div>
+                     <div className="w-[320px] h-[400px] flex flex-col">
+                        <div className="w-full h-[220px] bg-[#ECECEC50] flex flex-col gap-3 rounded-t-[10px] p-5">
+                            <div>
+                                <h1 className="font-semibold text-[24px]">Okay lang</h1>
+                                <div className="flex items-center gap-1">
+                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M6 11C8.76142 11 11 8.76142 11 6C11 3.23858 8.76142 1 6 1C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11Z" stroke="black" stroke-opacity="0.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M6 3V6L5 8" stroke="black" stroke-opacity="0.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                    <span className="font-light text-black/50 text-[12px]">September 09, 2026</span>
+                                </div>
+                            </div>
+                            <p className="text-[12px]">A gentle space where people can check in with themselves without pressure, explanation, or judgment.</p>
+                        </div>
+                        <div className="w-full h-full bg-[#DEDEDE] rounded-b-[10px]">
+
+                        </div>
+                    </div>
+                 <div className="w-[320px] h-[400px] flex flex-col">
+                        <div className="w-full h-[220px] bg-[#ECECEC50] flex flex-col gap-3 rounded-t-[10px] p-5">
+                            <div>
+                                <h1 className="font-semibold text-[24px]">Okay lang</h1>
+                                <div className="flex items-center gap-1">
+                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M6 11C8.76142 11 11 8.76142 11 6C11 3.23858 8.76142 1 6 1C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11Z" stroke="black" stroke-opacity="0.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M6 3V6L5 8" stroke="black" stroke-opacity="0.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                    <span className="font-light text-black/50 text-[12px]">September 09, 2026</span>
+                                </div>
+                            </div>
+                            <p className="text-[12px]">A gentle space where people can check in with themselves without pressure, explanation, or judgment.</p>
+                        </div>
+                        <div className="w-full h-full bg-[#DEDEDE] rounded-b-[10px]">
+
+                        </div>
+                    </div>
+                </div>
+            </BentoCard>  
+            <BentoCard title={"Projects"} icon={projectsIcon} className="w-full min-h-[300px] relative gap-8"> 
+                
+
+            </BentoCard>          
            <ContactMe className="min-h-[200px]"/>
         </div>
     )
