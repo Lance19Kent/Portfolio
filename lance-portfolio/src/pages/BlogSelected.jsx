6c6f6b25aa5790fcbcc89fa774fb90153ae141c3
Lance Kent <lancemagollado46@gmail.com>
2026-03-16 21:28:47 +0800

    Blog-Selected Initialized

--- lance-portfolio/src/pages/BlogSelected.jsx
+++ lance-portfolio/src/pages/BlogSelected.jsx
@@ -1,14 +1,11 @@
 import BlogSubContent from "@/components/blog/BlogSubContent";
-import BentoCard from "../components/BentoCard";
+import BlogSelectedContent from "@/components/blog/BlogSelectedContent";
 
 function BlogSelected(){
     return(
-        <div className="w-full">
-            <div className="w-full grid grid-cols-[2fr_1fr]">
-                <div className="w-full h-screen bg-white">
-                </div>
-                <BlogSubContent/>
-            </div>
+        <div className="w-full grid grid-cols-[2fr_1fr] gap-3">
+            <BlogSelectedContent/>
+            <BlogSubContent/>
         </div>
     )
 }
