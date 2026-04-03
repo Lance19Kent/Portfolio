41b1a14f6b67de22f2797d2deaa826d6f0ae721d
Lance Kent <lancemagollado46@gmail.com>
2026-04-03 09:44:03 +0800

    Fixed Bug

--- lance-portfolio/src/pages/ProjectSelected.jsx
+++ lance-portfolio/src/pages/ProjectSelected.jsx
@@ -2,15 +2,25 @@ import ProjectSelectedContent from "@/components/projects/ProjectSelectedContent
 import ProjectSubContent from "@/components/projects/ProjectSubContent";
 import { useParams, Navigate } from "react-router-dom";
 import { projectsData } from "@/data";
+import { useRef, useEffect } from "react";
 function ProjectSelected(){
     const { slug } = useParams();
     const project = projectsData.find((b) => b.slug === slug);
+
+     const scrollContainerRef = useRef(null);
+       useEffect(() => {
+             window.scrollTo(0, 0); 
+             
+             if (scrollContainerRef.current) {
+                 scrollContainerRef.current.scrollTo(0, 0);
+             }
+         }, [slug]);
 
      if(!project) return <Navigate to="/projects" replace/>
 
      const sidebarProjects = projectsData.filter((p) => p.slug !== slug).slice(0,4);
     return(
-        <div className="grid lg:grid-cols-[2.5fr_1fr] grid-cols-1 px-3 lg:px-0 gap-3 ">
+        <div className="grid lg:grid-cols-[2.5fr_1fr] grid-cols-1 px-3 lg:px-0 gap-3 " ref={scrollContainerRef}>
             <div className="overflow-y-auto no-scrollbar lg:py-3 pt-3" key={slug}>
                 <ProjectSelectedContent/>
             </div>
