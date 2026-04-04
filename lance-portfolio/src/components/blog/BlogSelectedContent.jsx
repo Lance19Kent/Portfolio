1f2449e1f0af797f6dff03a5439af89c78efe599
Lance Kent <lancemagollado46@gmail.com>
2026-04-04 19:46:51 +0800

    Quote Modifications

--- lance-portfolio/src/components/blog/BlogSelectedContent.jsx
+++ lance-portfolio/src/components/blog/BlogSelectedContent.jsx
@@ -131,7 +131,7 @@ function BlogSelectedContent() {
               key={index}
               className="my-8 p-6 lg:p-8 bg-zinc-50 dark:bg-zinc-800/50 rounded-2xl border-l-4 border-[#468cfc]"
             >
-              <p className="font-medium lg:text-l text-lg italic text-zinc-900 dark:text-white leading-relaxed">
+              <p className="font-medium lg:text-lg text-base italic text-zinc-900 dark:text-white leading-relaxed">
                 "{block.text}"
               </p>
             </div>
